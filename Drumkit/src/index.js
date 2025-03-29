fcb5c53601f1d2e658298222c72d9c13d6247af9
Brian <musyimib449@gmail.com>
2025-03-29 23:18:58 +0300

    drumkit plan structure and styling

---
+++ Drumkit/src/index.js
@@ -0,0 +1,11 @@
+document.addEventListener('DOMContentLoaded', ()=>{
+
+
+})
+
+function tilbox(){
+    const tile = document.querySelectorAll('.tile')
+    tile.addEventListener('click', ()=>{
+        
+    })
+}
