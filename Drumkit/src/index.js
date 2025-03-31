0907b8abd3140a4b15a75e768ba715afd2ee5d6c
Brian <musyimib449@gmail.com>
2025-04-01 01:46:28 +0300

    atach souind to tile clicks

--- Drumkit/src/index.js
+++ Drumkit/src/index.js
@@ -1,11 +1,17 @@
 document.addEventListener('DOMContentLoaded', ()=>{
 
-
+    tilebox();
 })
 
-function tilbox(){
-    const tile = document.querySelectorAll('.tile')
-    tile.addEventListener('click', ()=>{
-        
+function tilebox(){
+    const tiles = document.querySelectorAll('.tile')
+    tiles.forEach(tile =>{
+        // get to each individual tile and query the element audio 
+        tile.addEventListener('click', ()=>{
+            let beat = tile.querySelector('audio')
+            beat.currentTime = 0; //reset so it plays from the start
+            beat.play().catch(error => console.error("Playback error:", error));
+
+        })
     })
 }
