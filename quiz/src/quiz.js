6d79f2aa4319a537a0bd01ec64157a3af346c0db
Brian <musyimib449@gmail.com>
2025-04-02 17:13:03 +0300

    trivia section added

--- quiz/src/quiz.js
+++ quiz/src/quiz.js
@@ -27,9 +27,30 @@ function tiles(){
         // tile event
         tile.addEventListener('click', ()=>{
             
-            
-            
+            const choiceData = tile.dataset.type.toUpperCase()
+            const pop = document.querySelector('#pop-up');
+
+            //enable pop up in relation to choice made
+            const choice = document.querySelector('.choice')      
+            modal(choiceData)
             
         })
     })
+}
+
+function modal(choiceData){
+    const pop = document.querySelector('#pop-up');
+    const close = document.querySelector(".close");
+    const start = document.querySelector(".start")
+    const choice = document.querySelector('.choice')
+
+    pop.style.display = 'block';
+    choice.textContent = choiceData
+    close.addEventListener('click', ()=>{
+        pop.style.display = 'none';
+    })
+
+    // start.addEventListener('click', ()=>{
+    //     // redirect to page trivia referenced to the choice made
+    // })
 }
