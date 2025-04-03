f9c160e2a36e092d4c9bd40bbed5b7da8818ed92
Brian <musyimib449@gmail.com>
2025-04-03 14:08:56 +0300

    update quiz.js for trivia

--- quiz/src/quiz.js
+++ quiz/src/quiz.js
@@ -27,12 +27,13 @@ function tiles(){
         // tile event
         tile.addEventListener('click', ()=>{
             
-            const choiceData = tile.dataset.type.toUpperCase()
+            const choiceData = tile.dataset.type
             const pop = document.querySelector('#pop-up');
 
             //enable pop up in relation to choice made
             const choice = document.querySelector('.choice')      
             modal(choiceData)
+            trivia(choiceData)
             
         })
     })
@@ -45,12 +46,56 @@ function modal(choiceData){
     const choice = document.querySelector('.choice')
 
     pop.style.display = 'block';
-    choice.textContent = choiceData
+    choice.textContent = choiceData.toUpperCase()
     close.addEventListener('click', ()=>{
         pop.style.display = 'none';
     })
 
-    // start.addEventListener('click', ()=>{
-    //     // redirect to page trivia referenced to the choice made
-    // })
+    start.addEventListener('click', ()=>{
+        // redirect to page trivia referenced to the choice made
+        window.location.href = './public/trivia.html'
+    })
+}
+
+function trivia(choice){
+    const quizes = "http://localhost:3000/quizes";
+    //retrive data
+    fetch(quizes)
+    .then((res => res.json()))
+    .then((data => {
+
+        const currentTile = data[choice]; //accessing topic choices
+        let quiz = [] 
+        // access current tile array
+        currentTile.forEach(quizData => {
+            // console.log(quizData.quiz)
+            // console.log(quizData.option)
+            // console.log(quizData.answer)
+
+            // store in quiz array loop through indexes,foward through event nextbtn
+                   
+            quiz.push(quizData.quiz) 
+                       
+            })
+            foward(quiz)
+        }))
+    .catch((err => console.log('Error fetching data:', err)))
+ 
+}
+
+function display(quiz,index){
+    const question = document.querySelector('.questions');
+    if(quiz.length > 0){
+        question.textContent = quiz[index]
+    }
+}
+//next button event
+function foward(quiz){
+    let index = 0
+    const next = document.querySelector('.next')
+    next.onclick = () => {
+        index = (index + 1)
+        if(index < quiz.length)
+        display(quiz,index)
+}
 }
